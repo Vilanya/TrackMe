f5cd57158e49d92ce113ac394d00945620bf21bf
Vilanya <rratnayakemudiy@deakin.edu.au>
2020-07-18 21:03:45 +1000

    ENHANCE: Add new device

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const devices = [];
-
-devices.push({ user: "Mary", name: "Mary's iPhone" }); 
-devices.push({ user: "Alex", name: "Alex's Surface Pro" }); 
-devices.push({ user: "Mary", name: "Mary's MacBook" });
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
 devices.forEach(function(device) { 
     $('#devices tbody').append(`
@@ -11,4 +7,17 @@ devices.forEach(function(device) {
             <td>${device.name}</td>
         </tr>`
     );
+});
+
+$('#add-device').on('click', function() { 
+    const user = $('#user').val();
+    const name = $('#name').val(); 
+    devices.push({ user, name }); 
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href = 'device-list.html';
+});
+
+$('#send-command').on('click', function() { 
+    const command = $('#command').val(); 
+    console.log(`command is: ${command}`);
 });
