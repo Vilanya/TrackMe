4aa848761932b00b42d48102c968bbad2efa6d92
Vilanya <rratnayakemudiy@deakin.edu.au>
2020-08-12 22:58:51 +1000

    integrate mqtt sensor data

--- mqtt/mqtt.js
+++ mqtt/mqtt.js
@@ -1,7 +1,12 @@
 const mqtt = require('mqtt');
 const express = require('express');
 
+const randomCoordinates = require('random-coordinates');
+const rand = require('random-int');
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const bodyParser = require('body-parser'); 
 
@@ -21,11 +26,38 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+const Device = require('./models/device');
+
 const client = mqtt.connect("mqtt://broker.hivemq.com:1883");
 
 client.on('connect', () => { 
     console.log('mqtt connected');
+    client.subscribe('/sensorData');
 });
+
+client.on('message', (topic, message) => { 
+    if (topic == '/sensorData') {
+        const data = JSON.parse(message);
+
+        Device.findOne({"name": data.deviceId }, (err, device) => { 
+            if (err) {
+                console.log(err) 
+            }
+
+            const { sensorData } = device;
+            const { ts, loc, temp } = data;
+
+            sensorData.push({ ts, loc, temp }); 
+            device.sensorData = sensorData;
+
+            device.save(err => { 
+                if (err) {
+                    console.log(err) 
+                }
+            }); 
+        });
+    } 
+});     
 
 app.post('/send-command', (req, res) => { 
     const { deviceId, command } = req.body; 
@@ -33,6 +65,22 @@ app.post('/send-command', (req, res) => {
     client.publish(topic, command, () => {
         res.send('published new message'); 
     });
+});
+
+app.put('/sensor-data', (req, res) => { 
+    const { deviceId } = req.body;
+
+    const [lat, lon] = randomCoordinates().split(", "); 
+    const ts = new Date().getTime();
+    const loc = { lat, lon };
+    const temp = rand(20, 50);
+
+    const topic = '/sensorData';
+    const message = JSON.stringify({ deviceId, ts, loc, temp });
+
+    client.publish(topic, message, () => { 
+        res.send('published new message');
+    }); 
 });
 
 app.listen(port, () => { 
